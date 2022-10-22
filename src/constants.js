6e61c29640292ee42ad8ac1da75cff47ef114bc6
Selfshah <newaj.tamil125@gmail.com>
2022-10-22 10:20:59 +0600

    first commit

---
+++ src/constants.js
@@ -0,0 +1,27 @@
+export const APP_CONSTANTS = {
+
+    ALCHEMY_KEY:
+   
+      "wss://neat-weathered-friday.matic-testnet.discover.quiknode.pro/2d9b3f54e403d4f212df9f10b6cc49b6af7e35b4/",
+   
+    CONTRACT_ADDRESS: "0x83c9aEdC4073d2F3251F6082ec0E95A6cc88E138",
+   
+    OPENSEA_ASSETS_URL: "https://testnets.opensea.io/assets/mumbai",
+   
+    CLIENT_ID: "8dUKXLOmVMxcgP6HdMIUPu2acOiItM1p",
+   
+    RPC_TARGET: "https://mainnet.infura.io/v3/776218ac4734478c90191dde8cae483c",
+   
+    APP_LOGO: "https://i.ibb.co/2gmPw9f/twitter-Dapp-Logo.jpg",
+   
+    ADAPTER_TWITTER_CLIENT_VERIFIER: "twitter-testnet-dapp-gshdu",
+   
+    ADAPTER_TWITTER_CLIENT_ID: "8dUKXLOmVMxcgP6HdMIUPu2acOiItM1p",
+   
+    AUTH0_DOMAIN: "https://dev-ubcxyytm.us.auth0.com",
+   
+    BIRD_IMAGE: "https://i.ibb.co/42WNxRN/reshot-icon-bird-TL39-D5-P8-YH.png",
+   
+    REACT_APP_REFRESH_TIMER: 30,
+   
+   };
